d209702f7a6a5194bd5561a80b06873eba441ebc
katuysya <46689287+katuysya@users.noreply.github.com>
2019-02-11 22:49:26 +0300

    Create 6.0.py

---
+++ 6.0.py
@@ -0,0 +1,10 @@
+x = int(input('введите координату точки x'))
+y = int(input('введите координату точки y'))
+if x >= 0 and y >= 0:
+    print('1')
+elif x <= 0 and y >= 0:
+    print('2')
+elif x <= 0 and y <= 0:
+    print('3')
+elif x >= 0 and y <= 0:
+    print('4')

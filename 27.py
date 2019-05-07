5198557eae368d823cc3bb9ff437a55f5ebc978c
katuysya <46689287+katuysya@users.noreply.github.com>
2019-05-07 15:21:46 +0300

    Update 27.py

--- 27.py
+++ 27.py
@@ -11,4 +11,9 @@ if arr[5]>=2:
     l = math.factorial(2)
     c = math.factorial((arr[5]-2))
     counter = f//(c*l)
-print(arr[1]*arr[9] + arr[2]*arr[8] + arr[3]*arr[7] + arr[4]*arr[6] + counter)
+if arr[10]>=2:
+    v = math.factorial(arr[10])
+    r = math.factorial(2)
+    p = math.factorial((arr[10]-2))
+    counter1 = v//(r*p)    
+print(arr[1]*arr[9] + arr[2]*arr[8] + arr[3]*arr[7] + arr[4]*arr[6] + counter + counter1)
